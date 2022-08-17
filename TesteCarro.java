9204c8b5c21c852fcaa0e1418c6bc85e89c5ec1b
Your Name <you@example.com>
2022-08-16 23:49:43 -0300

    first commit

---
+++ TesteCarro.java
@@ -0,0 +1,14 @@
+
+package Aula6;
+
+public class TesteCarro {
+    
+    public static void main(String[] args) {
+        
+        Carro van = new Carro ("Fiat", "Ducato");
+        
+        System.out.println(van.numPassageiros);
+        
+    }
+    
+}
